bd8c2fcd6384e0f99fe6a9a3d867a211d6e93dcb
Maciej Szumielewicz <szumielewiczmaciejjacek@gmail.com>
2018-11-27 20:00:14 +0100

    Coffee Machine Application client init commit: 1. Project setup using Vue.js 2. Creation of two components for:  2.1 Main Panel used for coffee type selection visible for users  2.2 Diagnostic/Service Panel used to display machine status

---
+++ coffee-machine-client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MainPanel msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MainPanel from './components/MainPanel.vue'
+
+export default {
+  name: 'app',
+  components: {
+      MainPanel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
